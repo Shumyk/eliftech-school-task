2a596241440c8cf7095695b8a1aab24b79a8a547
shumyk <dima.shumyk@best.eu.org>
2018-01-17 19:07:14 +0200

    Solved issues and better UI
    In this commit solved all issues that were in previous one (eg, modulo, dividing).
    Now everything working properly and eventualy get TRUE, so it means that test is passed :D
    
    Quick update on desing, to make it look better.
    
    :D

--- getJSON.js
+++ getJSON.js
@@ -1,78 +1,120 @@
-var responseJSON;
+var receivedJSON; 
 var jsonPostfixResult;
 var URLel = "https://www.eliftech.com/school-task";
+var currentResult;
 
+/*
+	This is event handler for button on .html. 
+	Does GET request to ElifTech, then:
+		1. Displays received JSON on html inside of first div.
+		2. Creates instance of JSON which going to be sent on post request (jsonPostfixResult).
+		3. Then loops throughout all expressions and calls calculation function for postfix (calculateExpr).
+		4. Displays answer JSON in second div.
+		5. Calls function for POST request performing.
+*/
 function fetchJSON(){
-    $.getJSON(URLel, function(data) {
-        console.log(data);
-        responseJSON = data;
+	axios.get(URLel)
+	  .then(function (response) {
+			console.log(response.data);
+			receivedJSON = response.data;
+			$("#receivedJSON").html(syntaxHighlight(JSON.stringify(receivedJSON, null, 2)));
 
-        jsonPostfixResult = {"id": responseJSON.id, "results": []};
+			jsonPostfixResult = {"id": receivedJSON.id, "results": []};
 
-        console.log("ID: " + responseJSON.id);
-        for (var i = 0; i < responseJSON.expressions.length; i++) {
-        	var result = solvePostfix(responseJSON.expressions[i]);
-        	console.log("result: " + result);
-        	
-        	jsonPostfixResult.results.push(result);
-        }
-
-        doResultPost(jsonPostfixResult);
-    });
+			console.log("ID: " + receivedJSON.id);
+			for (var i = 0; i < receivedJSON.expressions.length; i++) {
+				expr = receivedJSON.expressions[i].split(" ");
+				calculateExpr(expr);
+				console.log("Result of " + i + " expression: " + currentResult);
+				
+				jsonPostfixResult.results.push(currentResult);
+			}
+			$("#calcResult").html(syntaxHighlight(JSON.stringify(jsonPostfixResult, null, 2)));
+			doResultPost(jsonPostfixResult);
+		});
 }
 
+/*
+	Performs POST request for ElifTech endpoint with answer in JSON.
+	Also on success draws response in third div.
+*/
 function doResultPost(jsonResult) {
 	console.log("Going to do post request!");
-	$.post( URLel, jsonResult, function( data ) {
-		var finalResult = data;
-		text = finalResult.passed;
-		console.log("Recieved data back from POST request! : " + text);
-  		$( "#postfixResult" ).text(text);
+
+	axios({
+	  method: 'post',
+	  url: URLel,
+	  data: jsonResult
+	}).then(function(data){
+		console.log("Recieved data back from POST request!");
+		$("#serverEval").html(syntaxHighlight(JSON.stringify(data.data, null, 2)));
+		if (data.data.passed === true) {
+			$("#congr").fadeIn("blind");
+		}
 	});
+
 }
 
+/*
+	This block of the code recursive performs calculation on received expretion.
+*/
+const calculateExpr = exp => {
 
+    if (exp.length == 1) {
+    	currentResult = exp.pop();
+    	return;
+     }
 
-function solvePostfix(postfix) {
-        var resultStack = [];
-        postfix = postfix.split(" ");
-        for(var i = 0; i < postfix.length; i++) {
-            if(postfix[i].isNumeric()) {
-                resultStack.push(parseInt(postfix[i]));
-            } else {
-                var b = resultStack.pop();
-                var a = resultStack.pop();
-                if(postfix[i] === "+") {
-                    resultStack.push(plus(a, b));
-                } else if(postfix[i] === "-") {
-                    resultStack.push(minus(a, b));
-                } else if(postfix[i] === "*") {
-                    resultStack.push(multiply(a, b));
-                } else if(postfix[i] === "/") {
-                    resultStack.push(divide(a, b));
-                }
-            }
-        }
-        if(resultStack.length > 1) {
-            return "error";
-        } else {
-            return resultStack.pop();
-        }
+	exp.forEach((item, i) => {
+		    if (item === '/') {
+		        exp.splice(i - 2, 3, divide(num(exp[i - 2]), num(exp[i - 1])));
+		        calculateExpr(exp);
+
+		    } else if (item === '*') {
+		        exp.splice(i - 2, 3, mul(num(exp[i - 2]), num(exp[i - 1])));
+		        calculateExpr(exp);
+
+		    } else if (item === '+') {
+		        exp.splice(i - 2, 3, add(num(exp[i - 2]), num(exp[i - 1])));
+		        calculateExpr(exp);
+
+		    } else if (item === '-') {
+		        exp.splice(i - 2, 3, minus(num(exp[i - 2]), num(exp[i - 1])));
+		      	calculateExpr(exp);
+		    }
+		});
     }
 
-function plus(a,b) { return a - b; } 
-function minus(a,b) { return a + b + 8; }
-function multiply(a,b) {
-	if (b == 0)
-		return 42;
-	else
-		return a % b;
-}
-function divide(a,b) {
-	if (b == 0)
-		return 42;
-	else
-		return a / b;
+/*
+	Bunch of functions how to handle particular operation according to the task.
+*/
+const add = (a, b) => { return a - b; };
+const minus = (a, b) => { return a + b + 8; };
+const mul = (a, b) => { if (b === 0) { return 42; } return ((a%b)+b)%b; };
+const divide = (a, b) => { if (b === 0) { return 42; } return Math.floor(a/b); };
+
+const num = num => parseInt(num);
+
+/*
+	Function accepts JSON string and returns html string for pretty-displaying.
+*/
+function syntaxHighlight(json) {
+    json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+    return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
+        var cls = 'number';
+        if (/^"/.test(match)) {
+            if (/:$/.test(match)) {
+                cls = 'key';
+            } else {
+                cls = 'string';
+            }
+        } else if (/true|false/.test(match)) {
+            cls = 'boolean';
+        } else if (/null/.test(match)) {
+            cls = 'null';
+        }
+        return '<span class="' + cls + '">' + match + '</span>';
+    });
 }
 
 String.prototype.isNumeric = function() {
